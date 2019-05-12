126481963b4d09d212798575c21c5596035ffa47
BruceQiu1996 <18852951552@163.com>
2019-05-12 18:43:48 +0800

    add express

---
+++ Svit/ConfigEditor/Helpers/CloseWindowService.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm.UI;
+using System.Windows;
+namespace ConfigEditor
+{
+    public interface ICloseWindowService
+    {
+        void Close();
+    }
+    public class CloseWindowService : ServiceBase, ICloseWindowService
+    {
+        public void Close()
+        {
+            if ((AssociatedObject is Window))
+                ((Window)AssociatedObject).Close();
+        }
+    }
+}
